6fbab227d7d57f245ce01d6bd24f7cf92950e394
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2024-07-30 22:01:24 +0100

    I18nService tests.

---
+++ src/test/java/com/jackalope/thumptest/service/I18nServiceTest.java
@@ -0,0 +1,36 @@
+/* Copyright (C) 2024 Jackalope Technologies Ltd - All Rights Reserved */
+package com.jackalope.thumptest.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class I18nServiceTest {
+    private I18nService i18nService;
+
+    @BeforeEach
+    void setup() {
+        i18nService = new I18nService();
+    }
+
+    @Test
+    void testGetBundle() {
+        ResourceBundle actualResourceBundle = i18nService.getBundle();
+
+        assertNotNull(actualResourceBundle);
+    }
+
+    @Test
+    void testGetString() {
+        var expected = "Clear";
+
+        var actual = i18nService.getString("button.clear");
+
+        assertEquals(expected, actual);
+    }
+
+}
